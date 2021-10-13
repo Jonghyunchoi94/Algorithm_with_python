22c635287db5214628297aec538d686b8c8b7d5d
jonghyun_choi <jonghyun2016@naver.com>
2021-10-13 18:25:32 +0900

    Solve 1 Problem

--- SWEA/모의SW역량테스트/벽돌깨기/s1.py
+++ SWEA/모의SW역량테스트/벽돌깨기/s1.py
@@ -1,49 +1,65 @@
 import sys
 sys.stdin = open('input.txt')
 
+from collections import deque
 from copy import deepcopy
-
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, 1, -1]
 
-def find_not_empty(have, column):
-    res = H - 1
-    for i in range(H):
-        if have[i][column]:
-            res = i
-            break
-    return res
-
-def change_data(visit_data, memory):
+def change_data(visit_data, before):
+    after = deepcopy(before)
     for r, c in visit_data:
-        memory[r][c] = 0
+        after[r][c] = 0
         idx = r - 1
         while idx >= 0:
-            if memory[idx][c] != 0:
-                memory[r][c], memory[idx][c] = memory[idx][c], memory[r][c]
+            if after[idx][c] != 0:
+                after[r][c], after[idx][c] = after[idx][c], after[r][c]
                 r = idx
             else:
                 break
             idx -= 1
-    return memory
+    return after
 
 
-def start_point(r, c, value):
-    global visited
-    visited.add((r, c))
-    for k in range(4):
-        next_r = r + dr[k]
-        next_c = c + dc[k]
-        if 0 <= next_r < H and 0 <= next_c < W and value > 1 and visit[next_r][next_c] == 0:
-            visit[next_r][next_c] = 1
-            start_point(next_r, next_c, value - 1)
-            visit[next_r][next_c] = 0
+def explode(before, row, column):
+    global visit_pos, visited
+    q = deque()
+    q.append((row, column))
+    while q:
+        r, c = q.popleft()
+        visited[r][c] = 1
+        visit_pos.add((r, c))
 
-def try_combination(tr, cur_data, col, cnt):
-    global visited, answer
-    visited = set()
-    if col > W:
+        if before[r][c] <= 1:
+            continue
+        else:
+            for k in range(4):
+                for power in range(1, before[r][c]):
+                    next_r = r + (dr[k] * power)
+                    next_c = c + (dc[k] * power)
+                    if 0 <= next_r < H and 0 <= next_c < W and visited[next_r][next_c] == 0 and before[next_r][next_c] != 0:
+                        q.append((next_r, next_c))
+
+    sort_visited = sorted(list(visit_pos), key=lambda x: (x[0], x[1]))
+    after = change_data(sort_visited, before)
+    return after, len(visit_pos)
+
+
+def find_not_empty(empty_data, column):
+    res = H
+    for i in range(H):
+        if empty_data[i][column]:
+            res = i
+            break
+    return res
+
+def try_position(tr, cur_data, cnt):
+    global answer, visit_pos, visited
+    visit_pos = set()
+    visited = [[0] * W for _ in range(H)]
+    if cnt == total:
+        answer = total
         return
 
     if tr == N:
@@ -52,22 +68,24 @@ def try_combination(tr, cur_data, col, cnt):
         return
 
     for i in range(W):
-        try_combination(tr, cur_data, col + 1, cnt)
-        change = deepcopy(cur_data)
-        start_point(find_not_empty(cur_data, col), col, cur_data[find_not_empty(cur_data, col)][col])
-        sort_visited = sorted(list(visited), key=lambda x: (x[0], x[1]))
-        new_data = change_data(sort_visited, change)
-        try_combination(tr + 1, new_data, col + 1, cnt + len(sort_visited))
-
-
+        if find_not_empty(cur_data, i) < H:
+            new = explode(cur_data, find_not_empty(cur_data, i), i)
+            try_position(tr + 1, new[0], cnt + new[1])
+    return
 
 T = int(input())
 
 for case in range(T):
     N, W, H = map(int, input().split())
-    data = [list(map(int, input().split())) for _ in range(H)]
-    visit = [[0] * W for _ in range(H)]
+    ball = [list(map(int, input().split())) for _ in range(H)]
     answer = 0
+    total = 0
+    for i in range(H):
+        for j in range(W):
+            if ball[i][j] != 0:
+                total += 1
+    try_position(0, ball, 0)
 
-    try_combination(0, data, 0, 0)
-    print(answer)
+    res = total - answer
+
+    print('#{} {}'.format(case + 1, res))
