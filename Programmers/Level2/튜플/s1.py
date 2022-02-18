e32055b83d23659df2e3358ec2a8f4bc954539dc
jonghyun_choi <jonghyun2016@naver.com>
2022-02-18 10:57:30 +0900

    Solve 2 Problems

---
+++ Programmers/Level2/튜플/s1.py
@@ -0,0 +1,18 @@
+s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
+
+
+def solution(s):
+    answer = []
+    s = s[2:-2]
+    s = s.split("},{")
+    s.sort(key=len)
+
+    for i in s:
+        temp = i.split(",")
+        for j in temp:
+            if int(j) not in answer:
+                answer.append(int(j))
+
+    return answer
+
+print(solution(s))
