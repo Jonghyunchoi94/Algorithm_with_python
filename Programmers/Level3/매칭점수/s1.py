9070bf613e256d5958e174a1f8727d8e32e51c59
jonghyun_choi <jonghyun2016@naver.com>
2022-03-17 15:59:55 +0900

    Modify 1 File Structure & Update README.md

--- Programmers/Level3/매칭점수/s1.py
+++ Programmers/Level3/매칭점수/s1.py
@@ -13,39 +13,34 @@ pages = [
     "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>",
     "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"
 ]
-
 import re
 
 def solution(word, pages):
     answer = 0
-    site_indexing = [0] * len(pages)
-    link_cnt = [0] * len(pages)
-    base_score = [0] * len(pages)
+    url_storage = [0] * len(pages)
+    basic_score = [0] * len(pages)
     link_score = [0] * len(pages)
     total_score = [0] * len(pages)
 
     for i in range(len(pages)):
-        master = re.findall("(content=)(\")(.+)(\")", pages[i])
-        site_indexing[i] = master[0][-2]
-        word_search = re.findall("(%s+[^a-zA-Z])" % word, pages[i], re.IGNORECASE)
-        # r"\b%s\b" %
-        print(word_search)
-        base_score[i] = len(word_search)
-    # print(site_indexing)
-    # print(base_score)
-    for i in range(len(pages)):
-        link = re.findall("(href=)(\")(.+)(\")", pages[i])
-        total_cnt = len(link)
-        for j in link:
-            link_cnt[site_indexing.index(j[-2])] += 1
-            tmp_score = base_score[i] / total_cnt
-            link_score[site_indexing.index(j[-2])] += tmp_score
-    # print(link_cnt)
-    # print(link_score)
+        master = re.search('<meta property="og:url" content="(\S+)"', pages[i]).group(1)
+        url_storage[i] = master
+
+        for str in re.findall(r'[a-zA-Z]+', pages[i].lower()):
+            if str == word.lower():
+                basic_score[i] += 1
 
     for i in range(len(pages)):
-        total_score[i] = base_score[i] + link_score[i]
-    # print(total_score)
+        link_ext = re.findall('<a href="(https://[\S]*)"', pages[i])
+        link_cnt = len(link_ext)
+
+        for link in link_ext:
+            if link in url_storage:
+                link_score[url_storage.index(link)] += (basic_score[i] / link_cnt)
+
+    for i in range(len(pages)):
+        total_score[i] = basic_score[i] + link_score[i]
+
     compare = -1
     for i, j in enumerate(total_score):
         if j > compare:
